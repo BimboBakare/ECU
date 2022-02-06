5826cd348f85e27416db12fd5c97dad4cd0bf4c4
BimboBakare <git@bimbobakare.com>
2022-02-06 05:21:32 +0000

    Update second.sh to take care of lack of response 	modified:   portfolio/week1/second.sh 	deleted:    portfolio/week3/megafoldermaker.sh

--- portfolio/week1/second.sh
+++ portfolio/week1/second.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
+# second.sh - receives input from the command line.
+# Author: Bimbo Bakare
+# Date:
+
+    # stores the argument received in a variable
+
+    mname="$1"
+
+    if (( $#<1 )); then
+
+       # if a null response is received
+
+        while [ "$mname" = "" ];
+        do
+             clear
+
+             read -p "Please type your first name : "  mname
+       
+       done
+   fi
+
 echo "Hi there!"
-echo "It's good to see you $1!"
+echo "It's good to see you $mname!"
+
 exit 0
 
+# References:
+# McKnight, Rob  "1.4 Write and execute simple shell scripts"
