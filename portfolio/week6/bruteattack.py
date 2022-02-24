1c22b33fb3595bf53983ccf38a774c0849563350
BimboBakare <97930998+BimboBakare@users.noreply.github.com>
2022-02-24 10:52:40 +0800

    Add files via upload
    import bcrypt to hash password

--- portfolio/week6/bruteattack.py
+++ portfolio/week6/bruteattack.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3 
-# bruteattack.py - python script for dbruteforce attack using combinations of letters in the alphabet & numbers
+# bruteattack.py - python script for dbruteforce attack using combinations of letters in the alphabet and numbers
 # Author Bimbo Bakare
 # Date 22Feb2022
 
@@ -8,12 +8,21 @@ from pkg_resources import WorkingSet
 import itertools
 import time
 import hashlib
+import bcrypt
+
 
 # Specifying the characters to use for the bruteforce attack, as opposed to a dictionary
 Alphabet = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_.!")
+
+# Receives passwordinput from user
 Password = input("What is your password?\n")
-# str = hashlib.sha256(mPassword.encode('utf-8'))
-# Password = str.hexdigest()
+
+# hashes the password using bcrypt
+hashed_password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+
+# convert to string with correct format
+string_password = hashed_password.decode('utf8')
+
 
 start = time.time()
 
@@ -38,14 +47,10 @@ for CharLength in range(15):
         i = i.replace("(", "")
         i = i.replace(")", "")
 
-        # str = hashlib.sha256(i.encode('utf-8'))
-        # ivv = str.hexdigest()
-
-        if i == Password:
+        if i == string_password:
             end = time.time()
             exp_time = end - start
             print("Found it in ", exp_time, " seconds and ", mcount, "attempts")
-
             print("That is ", mcount / exp_time, " attempts per second!")
             print(Password)
             input("TIRED? Press ENTER to END")
